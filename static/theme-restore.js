a415bc5ff36c183ba21847f0d800c8f53753b636
kurmachu <31492625+kurmachu@users.noreply.github.com>
2022-11-20 03:03:11 -0500

    Theming system fixes and our first dark theme, Firework!

--- static/theme-restore.js
+++ static/theme-restore.js
@@ -1,2 +1,5 @@
 if(!localStorage.getItem("theme")){localStorage.setItem("theme","/default/arithm.css")}
 document.getElementById("theme-zone").href = `/retro-docs-site/themes${localStorage.getItem("theme")}`
+if(localStorage.getItem("theme").startsWith("/dark")){ //For dark themes, make it flash black on nav and not white.
+	document.getElementsByTagName("html")[0].style.backgroundColor = "black"
+}
